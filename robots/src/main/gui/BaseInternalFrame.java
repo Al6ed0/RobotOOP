0144d824aba03f9be74ea349aaf4b6a9e8951f31
Al6ed0 <den06m@gmail.com>
2026-05-10 19:06:28 +0500

    2.2 task commit

---
+++ robots/src/main/gui/BaseInternalFrame.java
@@ -0,0 +1,32 @@
+package gui;
+
+import javax.swing.*;
+import javax.swing.event.InternalFrameAdapter;
+import javax.swing.event.InternalFrameEvent;
+
+public class BaseInternalFrame extends JInternalFrame {
+
+    public BaseInternalFrame(String title, boolean resizable,
+                             boolean closable, boolean maximizable, boolean iconifiable) {
+        super(title, resizable, closable, maximizable, iconifiable);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addInternalFrameListener(new InternalFrameAdapter() {
+            @Override
+            public void internalFrameClosing(InternalFrameEvent e) {
+                confirmClose();
+            }
+        });
+    }
+
+    private void confirmClose() {
+        int response = JOptionPane.showConfirmDialog(this,
+                "Вы действительно хотите закрыть окно '" + getTitle() + "'?",
+                "Подтверждение",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE);
+        if (response == JOptionPane.YES_OPTION) {
+            dispose();
+        }
+    }
+
+}
